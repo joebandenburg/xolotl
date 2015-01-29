33a3f541cc90c47d3ddad79a421e8b1c4ddc6cc8
assbanditman <seansaysmeh@gmail.com>
2015-01-29 09:19:14 +0000

    Updated ui in contact list

--- js/contacts.js
+++ js/contacts.js
@@ -6,15 +6,23 @@
         $scope.contacts = [
         {
             name: "Troy McClure",
-            number: "123976976876"
+            number: "123976976876",
+            lastMessage: "Hi, I'm Troy McCLure, you might rlorem sdfsdfs sdf sdf sdf sdfsdf sdfs dfs dfsdfsdfs"
+        },
+        {
+            name: "Joe Bandenburg",
+            number: "+44277234223",
+            lastMessage: "I'm a teapot, short and stout, here's my"
         },
         {
             name: "Sea Captain",
-            number: "456786767867"
+            number: "456786767867",
+            lastMessage: "Arrrgh, that be true"
         },
         {
             name: "",
-            number: "4576455464564"
+            number: "4576455464564",
+            lastMessage: "Is that you?"
         }];
 
         $scope.inputNumber = "";
@@ -30,6 +38,25 @@
                 $scope.contacts.push({name: "", number: num});
                 $scope.inputNumber = "";
             }
+        };
+
+        var hashCode = function(str) {
+          var hash = 0, i, chr, len;
+          if (str.length == 0) return hash;
+          for (i = 0, len = str.length; i < len; i++) {
+            chr   = str.charCodeAt(i);
+            hash  = ((hash << 5) - hash) + chr;
+            hash |= 0; // Convert to 32bit integer
+          }
+          return hash;
+        };
+
+        var randomHslString = function(seed) {
+            return "hsl(" + (hashCode(seed) % 360) + ", 50%, 50%)";
+        };
+
+        $scope.contactStyle = function(contact) {
+            return { "background" : randomHslString(contact.number)}
         };
 
         $scope.openConversation = function(contact) {
