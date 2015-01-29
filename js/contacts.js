7147b01caedd205a8de03893d93d2d0bc4eea693
assbanditman <seansaysmeh@gmail.com>
2015-01-29 17:07:47 +0000

    Added contact addition page

--- js/contacts.js
+++ js/contacts.js
@@ -2,33 +2,35 @@
     "use strict";
     var module = angular.module("XolotlContacts", []);
 
-    module.controller("ContactsController", function($scope, $location, $filter) {
-        $scope.contacts = [
-        {
-            name: "Troy McClure",
-            number: "123976976876",
-            lastMessage: "Hi, I'm Troy McCLure, you might rlorem sdfsdfs sdf sdf sdf sdfsdf sdfs dfs dfsdfsdfs"
-        },
-        {
-            name: "Joe Bandenburg",
-            number: "+44277234223",
-            lastMessage: "I'm a teapot, short and stout, here's my"
-        },
-        {
-            name: "Sea Captain",
-            number: "456786767867",
-            lastMessage: "Arrrgh, that be true"
-        },
-        {
-            name: "",
-            number: "4576455464564",
-            lastMessage: "Is that you?"
-        }];
+    module.controller("ContactsController", function($scope, $rootScope, $location, $filter, ColorGenerator) {
+        if (!$rootScope.contacts) {
+            $rootScope.contacts = [
+            {
+                name: "Troy McClure",
+                number: "123976976876",
+                lastMessage: "Hi, I'm Troy McCLure, you might rlorem sdfsdfs sdf sdf sdf sdfsdf sdfs dfs dfsdfsdfs"
+            },
+            {
+                name: "Joe Bandenburg",
+                number: "+44277234223",
+                lastMessage: "I'm a teapot, short and stout, here's my"
+            },
+            {
+                name: "Sea Captain",
+                number: "456786767867",
+                lastMessage: "Arrrgh, that be true"
+            },
+            {
+                name: "",
+                number: "4576455464564",
+                lastMessage: "Is that you?"
+            }];
+        }
 
         $scope.contextInput = "";
 
         $scope.$watch("contextInput", function(newValue, oldValue) {
-            $scope.filteredContacts = $filter("filter")($scope.contacts, $scope.contextInput);
+            $scope.filteredContacts = $filter("filter")($rootScope.contacts, $scope.contextInput);
         });
 
         var isValidNumber = function(number) {
@@ -37,41 +39,17 @@
         };
 
         $scope.addContact = function() {
-            var num = $scope.contextInput;
-            if (isValidNumber(num)) {
-                $scope.contacts.push({name: "", number: num});
-                $scope.contextInput = "";
-            }
-        };
-
-        var hashCode = function(str) {
-            var hash = 0;
-            var i = 0;
-            var chr = 0;
-            var len = 0;
-            if (str.length === 0) {
-                return hash;
-            }
-            for (i = 0, len = str.length; i < len; i++) {
-                chr = str.charCodeAt(i);
-                hash = ((hash << 5) - hash) + chr;
-                hash |= 0; // Convert to 32bit integer
-            }
-            return hash;
-        };
-
-        var randomHslString = function(seed) {
-            return "hsl(" + (hashCode(seed) % 360) + ", 50%, 50%)";
+            $location.path("/conversation/add/" + $scope.contextInput);
         };
 
         $scope.contactStyle = function(number) {
             return {
-                "background" : randomHslString(number)
+                "background" : ColorGenerator.randomHslString(number)
             };
         };
 
         $scope.openConversation = function(contact) {
-            $location.path("/conversation/" + contact.number);
+            $location.path("/contact/" + contact.number);
         };
 
         $scope.handleContext = function() {
